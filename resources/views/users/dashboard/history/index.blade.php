da8378872ba8cb4cb2c953a41d28989a10e59e70
Dika Saputra <69504128+RKaSaputra@users.noreply.github.com>
2024-06-27 03:10:46 +0700

    history user

--- resources/views/users/dashboard/history/index.blade.php
+++ resources/views/users/dashboard/history/index.blade.php
@@ -5,23 +5,25 @@
 @section('top','History Booking')
 
 @section('content')
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Lapangan</th>
-                <th>Status Pembayaran</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($bookings as $booking )
-                
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $booking->lapangan->title }}</td>
-                <td>Sudah Dibayar</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Lapangan</th>
+            <th>Waktu</th>
+            <th>Status Pembayaran</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($bookings as $booking)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $booking->lapangan->title }}</td>
+            <td>{{ $booking->schedule->formatted_time }}</td>
+            <td>{{ $booking->status }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
 @endsection
